44dd6193472d6171f03bb74210cfb58940fbadc1
ArnabKumarMishra <arnkmish@indiana.edu>
2015-04-09 02:57:44 -0400

    The first welcome to java text print program

---
+++ Welcome1.java
@@ -0,0 +1,14 @@
+// Welcome1.java
+// Text printing program
+
+public class Welcome1{
+
+	// main method begins execution of Java applications
+	
+	public static void main(String[] args){
+
+		System.out.println("Welcome to Java programming!");
+
+	}// end of main method
+
+}// end of class Welcome1
