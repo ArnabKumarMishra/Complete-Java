a4d8cbc97c2166a4af64d5925a5c31f11031e248
ArnabKumarMishra <arnkmish@indiana.edu>
2015-04-10 01:11:09 -0400

    Formatting with printf

---
+++ Welcome4.java
@@ -0,0 +1,13 @@
+// Welcome4.java
+// Formatting with printf()
+
+public class Welcome4{
+
+	// main method begins the execution of Java Programs
+	public static void main(String[] args){
+
+		System.out.printf("%s%n%s%n","Welcome to", "Java Programming");
+
+	}// end of main class
+
+}// end of Welcome4 class
