84d004b0cff64aa2802ed59f86d17727eee63c3e
ArnabKumarMishra <arnkmish@indiana.edu>
2015-04-10 01:02:31 -0400

    Text output program, introduction to new line characters

---
+++ Welcome3.java
@@ -0,0 +1,15 @@
+// Welcome3.java
+// Text output program
+// Introduction to new line character
+
+public class Welcome3{
+
+	// main method starts execution of Java Programs
+
+	public static void main(String[] args){
+
+		System.out.println("Welcome \nto \nJava \nProgramming");
+
+	}// end of main method
+
+}// end of Welcome3 class
