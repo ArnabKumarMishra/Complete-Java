356bb4e6453a9267b5495d1b472d2666a1dfbcbd
ArnabKumarMishra <arnkmish@indiana.edu>
2015-04-10 00:55:29 -0400

    A similar welcome program that outputs the same welcome message as the previous Welcome1.java program, but by using multiple statements.

---
+++ Welcome2.java
@@ -0,0 +1,15 @@
+// Welcome2.java 
+// Same as Welcome1.java, just the output string is generated using multiple statements, rather than using just one statement.
+
+public class Welcome2{
+
+	// main method begins the execution of Java Applications
+
+	public static void main(String[] args){
+
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming");
+
+	}// end of main method 
+
+}// end of Welcome2 class
