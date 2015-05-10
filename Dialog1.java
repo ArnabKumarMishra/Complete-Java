3b0649b84735dab5f1907982fe116925be402996
ArnabKumarMishra <arnkmish@indiana.edu>
2015-05-10 14:34:58 -0400

    Dialog box GUI example

---
+++ Dialog1.java
@@ -0,0 +1,14 @@
+// Dialog1.java
+// Using JOptionPane to display message inside a dialogbox
+import javax.swing.JOptionPane;
+
+public class Dialog1{
+
+	public static void main(String[] args){
+
+		// Display a dialog with a message
+		JOptionPane.showMessageDialog(null, "Welcome to Java");
+
+	} // End of main
+
+} // End of class Dialog1
